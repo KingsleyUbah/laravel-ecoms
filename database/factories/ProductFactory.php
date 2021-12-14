7c4d844b8a651dcda7d371462018f78d2d64872a
Kingsley Ubah <ubahkingsley4@gmail.com>
2021-12-14 17:06:55 +0100

    Making initial commit. Product and User models added and seeded in DB

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $name = $this->faker->text(25);
+        $price = $this->faker->numberBetween($min = 100, $max = 900);
+
+        return [
+            'name' => $name,
+            'slug' => Str::slug($name),
+            'description' => $this->faker->text(100),
+            'image_name' => $this->faker->imageUrl($width = 140, $height = 280),     
+            'price' => $price,     
+            'discount' => $this->faker->numberBetween($min = 0, $max = 100),
+            'sale_price' => $price - 50,
+        ];
+    }
+}
---
+++ resources/js/components/AddToCart.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <button class="btn btn-warning" v-on:click.prevent="addProductToCart">
+            Add to Cart
+        </button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+
+        },
+        props: ['productId', 'userId'],
+        methods: {
+            addProductToCart() {
+                if(this.userId == 0) {
+                    this.$toastr.e('You need to login to add this product to the cart');
+                    return;
+                }
+            }
+        },
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
